49c3a351f6c175f6d5c6947febee47fd8177fdd1
Amogh <amoghagarwal1809555@gmail.com>
2026-02-10 11:30:15 -0800

    Latest Backend Changes

--- BACKEND/src/utils/response.js
+++
@@ -1,3 +0,0 @@
-function ok(res, data) { return res.json({ status: "success", ...data }); }
-function fail(res, message, code=400) { return res.status(code).json({ status: "error", message }); }
-module.exports = { ok, fail };
--- BACKEND_MVC/src/routes/landOffset.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/landOffset.controller');
-
-router.get('/land_offset', ctrl.getLandOffset);
-
-module.exports = router;
